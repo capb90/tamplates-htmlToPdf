819e8f11c75d76ce363da31fc3d8a84aa3b625f3
Carlos Andres Portillo B <capb.dev@gmail.com>
2023-11-22 14:40:14 -0500

    💥 NEW: Pdf actas

--- printall.sh
+++ printall.sh
@@ -1,3 +1,5 @@
 
 #Configurar el template de entrada y la salida
-weasyprint templates/convocatorias/index.html out/concovatoria.pdf
+##weasyprint templates/convocatorias/index.html out/concovatoria.pdf
+
+weasyprint templates/actas/index.html out/actas.pdf
